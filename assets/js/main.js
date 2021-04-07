3c77a2c71c672b26fa1d7226cb944e0fe826ff79
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-04-06 22:37:55 -0500

    se termino la sección de testimonials, contact y certificates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,32 @@
 AOS.init()
 
+// BUTTON TOP
 function scrollTop() {
 	const scrollTop = document.getElementById('scroll-top')
 	if (this.scrollY >= 560) scrollTop.classList.add('show-scroll')
 	else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
+
+const inputs = document.querySelectorAll('.input')
+
+// FORM CONTACT
+function focusFunc() {
+	let parent = this.parentNode
+	parent.classList.add('focus')
+}
+
+function blurFunc() {
+	let parent = this.parentNode
+	if (this.value == '') {
+		parent.classList.remove('focus')
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('focus', focusFunc)
+	input.addEventListener('blur', blurFunc)
+})
 
 // Animations
 gsap.to('.first', 1.9, { delay: 0.5, top: '-100%', ease: Expo.easeInOut })
