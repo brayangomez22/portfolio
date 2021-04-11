5f3509788867e2fd1ad71d09df73d1b29f9fbea4
andrescaro189 <andrescaro189@gmail.com>
2021-04-11 18:50:51 -0500

    Se genero pagina de Certificaciones

--- frontend/src/routes/App.js
+++ frontend/src/routes/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Particles from '../components/ParticlesJS'
 import Home from '../pages/Home'
 import NotFount from '../pages/NotFound'
+import Certificates from '../pages/Certificates'
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Particles />
 			<Switch>
 				<Route exact path="/" component={Home}></Route>
+				<Route exact path="/certificates" component={Certificates}></Route>
 				<Route component={NotFount}></Route>
 			</Switch>
 		</BrowserRouter>
