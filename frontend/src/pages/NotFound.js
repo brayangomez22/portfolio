c4155eb30b1ce90a33df554c4f22c3be61850fd6
andrescaro189 <andrescaro189@gmail.com>
2021-04-11 19:25:06 -0500

    se refactorizo el nor Fount

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -9,9 +9,13 @@ export default class NotFound extends Component {
                 <div className="wrap">
                     <div className=".box-notFount">
                         <div className="text animated pulse">
-                            <h1>Error 404</h1>
+                            <h1> HTTP 404 Not Found</h1>
                         </div>
-                        <p className="text p__error--404">HTTP 404 Not Found o HTTP 404 No encontrado es un código de estado HTTP que indica que el host ha sido capaz de comunicarse con el servidor, pero no existe el recurso que ha sido pedido. Este error no debe ser confundido con "servidor web no encontrado" o errores similares en los que se indica que no se ha podido realizar la conexión con el servidor.</p>
+                        <p className="text p__error--404">
+                        HTTP 404 Not Found o HTTP 404 No encontrado es un código de estado HTTP que indica que el host ha sido capaz de
+                        comunicarse con el servidor, pero no existe el recurso que ha sido pedido. Este error no debe ser confundido con
+                        "servidor web no encontrado" o errores similares en los que se indica que no se ha podido realizar la conexión con
+                        el servidor.</p>
                         <div className="text animated wobble">
                             <img src={tristeIcon} alt="triste" />
                         </div>
